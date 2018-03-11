5a7cb339a6e01e2bb2606f8dc70360f09a84a328
Lordjette Leigh Lecaros <32612885+lordjette@users.noreply.github.com>
2018-03-11 22:15:33 +0800

    Add files via upload

--- admin/controller/mail/customer.php
+++ admin/controller/mail/customer.php
@@ -1,5 +1,5 @@
 <?php
-class ControllerMailAffiliate extends Controller {
+class ControllerMailCustomer extends Controller {
 	public function approve(&$route, &$args, &$output) {
 		$this->load->model('customer/customer');
 		
@@ -17,7 +17,7 @@ class ControllerMailAffiliate extends Controller {
 				$store_name = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
 				$store_url = HTTP_CATALOG . 'index.php?route=account/login';
 			}
-
+			
 			$this->load->model('localisation/language');
 			
 			$language_info = $this->model_localisation_language->getLanguage($customer_info['language_id']);
@@ -30,13 +30,13 @@ class ControllerMailAffiliate extends Controller {
 			
 			$language = new Language($language_code);
 			$language->load($language_code);
-			$language->load('mail/affiliate_approve');
-							
-			$subject = sprintf($language->get('text_subject'), $store_name);
-			
-			$data['text_welcome'] = sprintf($language->get('text_welcome'), $store_name);
+			$language->load('mail/customer_approve');
 						
-			$data['login'] = $store_url . 'index.php?route=account/login';
+			$subject = sprintf($language->get('text_subject'), $store_name);
+								
+			$data['text_welcome'] = sprintf($language->get('text_welcome'), $store_name);
+				
+			$data['login'] = $store_url . 'index.php?route=account/login';	
 			$data['store'] = $store_name;
 	
 			$mail = new Mail($this->config->get('config_mail_engine'));
@@ -51,8 +51,8 @@ class ControllerMailAffiliate extends Controller {
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender($store_name);
 			$mail->setSubject($subject);
-			$mail->setText($this->load->view('mail/affiliate_approve', $data));
-			$mail->send();
+			$mail->setText($this->load->view('mail/customer_approve', $data));
+			$mail->send(); 
 		}
 	}
 	
@@ -86,15 +86,15 @@ class ControllerMailAffiliate extends Controller {
 
 			$language = new Language($language_code);
 			$language->load($language_code);
-			$language->load('mail/affiliate_deny');
+			$language->load('mail/customer_deny');
 				
-			$subject = sprintf($language->get('text_subject'), $store_name);	
+			$subject = sprintf($language->get('text_subject'), $store_name);
 				
 			$data['text_welcome'] = sprintf($language->get('text_welcome'), $store_name);
-			
+				
 			$data['contact'] = $store_url . 'index.php?route=information/contact';	
 			$data['store'] = $store_name;
-			
+
 			$mail = new Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
@@ -107,7 +107,7 @@ class ControllerMailAffiliate extends Controller {
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender($store_name);
 			$mail->setSubject($subject);
-			$mail->setText($this->load->view('mail/affiliate_deny', $data));
+			$mail->setText($this->load->view('mail/customer_deny', $data));
 			$mail->send();
 		}
 	}
