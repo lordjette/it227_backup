368ae0a00dc97f228e438c5eaf83d6187bbaec3b
Lordjette Leigh Lecaros <32612885+lordjette@users.noreply.github.com>
2018-03-11 22:39:35 +0800

    Add files via upload

--- admin/controller/extension/total/sub_total.php
+++ admin/controller/extension/total/sub_total.php
@@ -1,16 +1,16 @@
 <?php
-class ControllerExtensionTotalCoupon extends Controller {
+class ControllerExtensionTotalSubTotal extends Controller {
 	private $error = array();
 
 	public function index() {
-		$this->load->language('extension/total/coupon');
+		$this->load->language('extension/total/sub_total');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('setting/setting');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('total_coupon', $this->request->post);
+			$this->model_setting_setting->editSetting('total_sub_total', $this->request->post);
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
@@ -37,34 +37,34 @@ class ControllerExtensionTotalCoupon extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/total/coupon', 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('extension/total/sub_total', 'user_token=' . $this->session->data['user_token'], true)
 		);
 
-		$data['action'] = $this->url->link('extension/total/coupon', 'user_token=' . $this->session->data['user_token'], true);
+		$data['action'] = $this->url->link('extension/total/sub_total', 'user_token=' . $this->session->data['user_token'], true);
 
 		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=total', true);
 
-		if (isset($this->request->post['total_coupon_status'])) {
-			$data['total_coupon_status'] = $this->request->post['total_coupon_status'];
+		if (isset($this->request->post['total_sub_total_status'])) {
+			$data['total_sub_total_status'] = $this->request->post['total_sub_total_status'];
 		} else {
-			$data['total_coupon_status'] = $this->config->get('total_coupon_status');
+			$data['total_sub_total_status'] = $this->config->get('total_sub_total_status');
 		}
 
-		if (isset($this->request->post['total_coupon_sort_order'])) {
-			$data['total_coupon_sort_order'] = $this->request->post['total_coupon_sort_order'];
+		if (isset($this->request->post['sub_total_sort_order'])) {
+			$data['sub_total_sort_order'] = $this->request->post['sub_total_sort_order'];
 		} else {
-			$data['total_coupon_sort_order'] = $this->config->get('total_coupon_sort_order');
+			$data['sub_total_sort_order'] = $this->config->get('sub_total_sort_order');
 		}
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('extension/total/coupon', $data));
+		$this->response->setOutput($this->load->view('extension/total/sub_total', $data));
 	}
 
 	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/total/coupon')) {
+		if (!$this->user->hasPermission('modify', 'extension/total/sub_total')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 
