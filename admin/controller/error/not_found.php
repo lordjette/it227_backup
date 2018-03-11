b2e812ea70db6ca3ab8d3bf53e4bb1eaad8af2ea
Lordjette Leigh Lecaros <32612885+lordjette@users.noreply.github.com>
2018-03-11 22:12:49 +0800

    Add files via upload

--- admin/controller/error/not_found.php
+++ admin/controller/error/not_found.php
@@ -1,8 +1,8 @@
 <?php
-class ControllerErrorPermission extends Controller {
+class ControllerErrorNotFound extends Controller {
 	public function index() {
-		$this->load->language('error/permission');
-
+		$this->load->language('error/not_found');
+		
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$data['breadcrumbs'] = array();
@@ -14,13 +14,13 @@ class ControllerErrorPermission extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link($this->request->get['route'], 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('error/not_found', 'user_token=' . $this->session->data['user_token'], true)
 		);
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('error/permission', $data));
+		$this->response->setOutput($this->load->view('error/not_found', $data));
 	}
 }
